1a67cc89e4b44322d908878a2b078377c8993f6d
Jake Williams <jakewilliams530@gmail.com>
2023-02-16 12:52:40 -0500

    About Component

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,12 +1,12 @@
 import React, {useState, useEffect} from 'react'
-import { useScrollHandler } from './scroll'
+import frame from '../images/frame.png'
 
 const About = () => {
 
     useEffect(() => {
         document.addEventListener('scroll', () => {
             const position = window.scrollY
-            const opacity = position * .004
+            const opacity = position * .0015
             console.log(opacity);
 
             document.getElementById('App').style.backgroundColor = `rgba(24, 24, 24, ${opacity.toString()})`
@@ -20,7 +20,16 @@ const About = () => {
     })
 
   return (
-    <div id = 'about' className = 'about'>About</div>
+    <div id = 'about' className = 'about'>
+        <div className = 'about-left'>
+            <img src = {frame} alt = 'My Photo'/>
+        </div>
+        <div className = 'about-right'>
+            <span className = 'about-title'>Hi, I'm Jake.</span>
+            <span className = 'about-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+        </div>
+
+    </div>
   )
 }
 
