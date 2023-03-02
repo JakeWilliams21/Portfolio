7502659694c075ef303fee2b753df7e7668f9364
Jake Williams <jakewilliams530@gmail.com>
2023-03-01 21:27:15 -0500

    Blurb added and font adjusted

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -43,7 +43,7 @@ const About = () => {
         </div>
         <div className = 'about-right'>
             <span className = 'about-title'>Hi, I'm Jake.</span>
-            <span className = 'about-paragraph'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span>
+            <span className = 'about-paragraph'>I'm a full stack web developer based in Columbus, Ohio, with a passion for science-fiction, music, and design. I love creating applications inspired by these interests using a variety of technologies like HTML, CSS, JavaScript, and PHP, as well as frameworks like React and Angular. When I'm not coding, I am attending concerts, reading sci-fi novels, and designing visually appealing applications. Let's collaborate to bring your ideas to life!</span>
         </div>
 
     </div>
